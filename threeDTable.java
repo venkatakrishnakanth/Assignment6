99229f95ed2ab4d16a927ae5e84e931bac569168
venkatakrishnakanth <venkatakrishnakanth@gmail.com>
2017-06-28 15:32:12 -0400

    Assignment6

--- threeDTable.java
+++ threeDTable.java
@@ -20,7 +20,7 @@ public class threeDTable {
 		
 		for(int i=1;i<11;i++){
 			
-			for(int j=i; j<11;j++){
+			for(int j=1; j<11;j++){
 				System.out.println(num+"*"+i+"*"+j+" = "+Integer.toString(num*i*j));
 			}
 		}
